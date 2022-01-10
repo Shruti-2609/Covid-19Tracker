beb3d0e34dda9c939f394c219e8dd336820fe3f2
Shruti-2609 <shrutisaloni3@gmail.com>
2022-01-11 01:00:01 +0530

    initial commit

---
+++ build/precache-manifest.c21c434cc6122c26a9c08ed2521b291a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7603c9aea55403df2c210b619cf8049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04f3a0959ab7671cc978",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "a9fde8d5903b1c9a5cd8",
+    "url": "/static/css/main.0c70f6e6.chunk.css"
+  },
+  {
+    "revision": "04f3a0959ab7671cc978",
+    "url": "/static/js/2.a860358b.chunk.js"
+  },
+  {
+    "revision": "9922ea9069e02abaf7aeceb5abadc3c8",
+    "url": "/static/js/2.a860358b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9fde8d5903b1c9a5cd8",
+    "url": "/static/js/main.a378a897.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
